641549716d25f01a495bd27865bd19aa0a705325
I-L9614 <leviitamar034@gmail.com>
2026-02-26 11:04:44 +0200

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 function Navbar() {
   return (
     <nav className='navbar'>
-        <img src="../images/shield.png" alt="" />
+        <img src="/shield.png" alt="logo" />
         <h1 className='logo'>Bomb Detection Application</h1>
         <p className='status'>Simulation Active</p>
     </nav>
